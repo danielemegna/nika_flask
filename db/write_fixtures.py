49df42968f1970569065aeb9cb3f0fafe8f6ba4d
Daniele Megna <daniele.megna@casavo.com>
2022-06-21 10:49:57 +0200

    Add table truncate commands on write_fixtures

--- db/write_fixtures.py
+++ db/write_fixtures.py
@@ -1,7 +1,9 @@
+import sqlite3
 from nika.ports.repository.sqlite_listing_repository import *
 
 def run(sqlite_filepath):
     print(f'Writing some fixture data into {sqlite_filepath} db file ...')
+    __truncate_tables(['listings'], sqlite_filepath)
 
     listing_repository = SqliteListingRepository(sqlite_filepath)
     __listing_candidates(listing_repository)
@@ -9,7 +11,7 @@ def run(sqlite_filepath):
     print('Done.')
 
 def __listing_candidates(listing_repository):
-    print('ListingCandidate ...')
+    print('Writing listing candidates ...')
     candidates = [
         ListingCandidate(
             property_transaction_id="22_12345",
@@ -32,3 +34,10 @@ def __listing_candidates(listing_repository):
     ]
     for c in candidates:
         listing_repository.add_candidate(c)
+
+def __truncate_tables(tables, sqlite_filepath):
+    print('Truncate tables ...')
+    db = sqlite3.connect(sqlite_filepath)
+    for table in tables:
+        db.execute(f"delete from {table}")
+    db.commit()
